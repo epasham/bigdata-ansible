b570db0b06fc2e675738dff092239fc6f5dbd375
Kwangsiklee <lks21c@gmail.com>
2018-08-02 15:03:56 +0900

    yes

---
+++ atlas-prd/download_and_build_binary.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+. script/env.sh
+rm -rf binary
+git clone https://github.com/apache/atlas binary
+cd binary && mvn clean -DskipTests package -Pdist
+cp distro/target/apache-atlas-$ATLAS_VERSION-bin.tar.gz .
