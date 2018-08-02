f66cff468a1687a6609126edd8e6bb8c537b0a9e
Kwangsiklee <lks21c@gmail.com>
2018-08-02 17:47:07 +0900

    yes

--- redash-prd/rsync_redash.sh
+++ redash-prd/rsync_redash.sh
@@ -7,7 +7,8 @@ echo "Redash BINARY SYNC START--------------------------------------------------
 for nodename in mpredash
 do
     echo "Redash SYNC in $nodename -------------------------------------------------------------------------"
-    rsync -rv -e ssh --delete script suser@$nodename:/home/suser/redash
+    rsync -rv -e ssh --delete script/*.sh suser@$nodename:/home/suser/redash
+    rsync -rv -e ssh --delete script/Dockerfile suser@$nodename:/home/suser/redash
 done
 
 echo "-------------------------------------------------------------------end  `date +%Y/%m/%d-%H:%M`"
