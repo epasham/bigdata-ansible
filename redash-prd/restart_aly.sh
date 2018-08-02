f1424f4ec0bde106369bdeebfa9436e754cfafe9
Kwangsiklee <lks21c@gmail.com>
2018-08-02 18:02:09 +0900

    yes

---
+++ redash-prd/restart_aly.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ssh suser@mpredash "cd /home/suser/redash && sh 004_restart.sh"
--- redash-prd/restart_private.sh
+++ redash-prd/restart_private.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-ssh suser@mpredash "cd /home/suser/redash && sh 004_restart.sh"
 ssh suser@mpredash02 "cd /home/suser/redash && sh 004_restart.sh"
