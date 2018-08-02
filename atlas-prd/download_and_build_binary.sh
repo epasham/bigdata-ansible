0d425d15f865749f8fc3ef5f5dfca4861b521a20
Kwangsiklee <lks21c@gmail.com>
2018-08-02 15:08:57 +0900

    yes

--- atlas-prd/download_and_build_binary.sh
+++ atlas-prd/download_and_build_binary.sh
@@ -5,3 +5,5 @@ rm -rf binary
 git clone https://github.com/apache/atlas binary
 cd binary && mvn clean -DskipTests package -Pdist
 cp distro/target/apache-atlas-$ATLAS_VERSION-bin.tar.gz .
+cd ..
+rm -rf binary
