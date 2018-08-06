57e5e8d55ecbf2ec5ddb0ac5fd31f5fbeae10472
Kwangsiklee <lks21c@gmail.com>
2018-08-06 15:44:43 +0900

    add unsafe_script

--- elasticsearch-prd/mpalyes/script/unsafe_script.sh
+++ elasticsearch-prd/mpalyes/script/unsafe_script.sh
@@ -6,6 +6,6 @@ echo "ES ALY delete script START------------------------------------------------
 
 for nodename in data1 data2 data3 data4 data5 data6 data7 data8 data9 data10 data11 data12 data13 data14 data15 data16 data17 data18 data19 data20 data21 data22 data23
 do
-    echo "delete directory in $nodename -------------------------------------------------------------------------"
-    rm -rf /$nodename/alyes_master/nodes/0/_state/global-10640.st
+    echo "run script in $nodename -------------------------------------------------------------------------"
+    cp -r /$nodename/alyes_master /$nodename/alyes_master_backup
 done
