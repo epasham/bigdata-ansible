fa92a68302894a67cd002cbc4a1b03ba6191f7fa
Kwangsiklee <lks21c@gmail.com>
2018-08-09 17:43:31 +0900

    yes

--- filebeat-prd/rsync_filebeat.sh
+++ filebeat-prd/rsync_filebeat.sh
@@ -9,21 +9,21 @@ echo "Filebeat BINARY SYNC START------------------------------------------------
 for nodename in mpalyes01
 do
     echo "Filebeat BINARY SYNC es in $nodename -------------------------------------------------------------------------"
-    rsync -rv -e ssh --delete ~/filebeat/filebeat-$ES_VERSION-linux-x86_64-master root@$nodename:/root/filebeat
+    rsync -rv -e ssh --delete ~/filebeat/filebeat-$ES_VERSION-linux-x86_64-es-master root@$nodename:/root/filebeat
     rsync -rv -e ssh --delete ~/filebeat/script root@$nodename:/root/filebeat
 done
 
 for nodename in mpalyes10 mpalyes11
 do
     echo "Filebeat BINARY SYNC es in $nodename -------------------------------------------------------------------------"
-    rsync -rv -e ssh --delete ~/filebeat/filebeat-$ES_VERSION-linux-x86_64-client root@$nodename:/root/filebeat
+    rsync -rv -e ssh --delete ~/filebeat/filebeat-$ES_VERSION-linux-x86_64-es-client root@$nodename:/root/filebeat
     rsync -rv -e ssh --delete ~/filebeat/script root@$nodename:/root/filebeat
 done
 
 for nodename in mpalyes02 mpalyes03 mpalyes04 mpalyes05 mpalyes06 mpalyes07 mpalyes08 mpalyes09 mpalyes12 mpalyes13 mpalyes14
 do
     echo "Filebeat BINARY SYNC es in $nodename -------------------------------------------------------------------------"
-    rsync -rv -e ssh --delete ~/filebeat/filebeat-$ES_VERSION-linux-x86_64-master-and-data root@$nodename:/root/filebeat
+    rsync -rv -e ssh --delete ~/filebeat/filebeat-$ES_VERSION-linux-x86_64-es-master-and-data root@$nodename:/root/filebeat
     rsync -rv -e ssh --delete ~/filebeat/script root@$nodename:/root/filebeat
 done
 
