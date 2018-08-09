f204377adc97c60fe3ffecca2f4ad7e1e1b3ce0b
Kwangsiklee <lks21c@gmail.com>
2018-08-09 17:46:57 +0900

    yes

--- filebeat-prd/ps.sh
+++ filebeat-prd/ps.sh
@@ -5,7 +5,7 @@ bin=`cd "$bin"; pwd`
 export SERVERLIST="${bin}/serverlist/serverlist_$1"
 
 for server in `cat "$SERVERLIST"`; do
-  ssh $server "ps -ef | grep filebeat"
+  ssh root@$server "ps -ef | grep filebeat"
 done
 
 wait
