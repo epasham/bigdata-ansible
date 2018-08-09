6bc2e00461f9fadcf03068ea4631b20314baa339
Kwangsiklee <lks21c@gmail.com>
2018-08-09 16:18:24 +0900

    yes

---
+++ elasticsearch-prd/mpalyes/make_filebeat_binary.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ansible-playbook ansible/elasticsearch.yml -i ansible/local_inventory
---
+++ metricbeat-prd/sbin/start.sh
@@ -0,0 +1,1 @@
+nohup /root/metricbeat/metricbeat -e -c /root/metricbeat/metricbeat.yml > /dev/null 2>&1 &
