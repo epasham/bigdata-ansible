dd982c03b2d6e850362bc8dec55f35bedc1caac7
Kwangsiklee <lks21c@gmail.com>
2018-08-09 16:52:22 +0900

    yes

--- filebeat-prd/rsync_filebeat.sh
+++ filebeat-prd/rsync_filebeat.sh
@@ -9,8 +9,8 @@ echo "Filebeat BINARY SYNC START------------------------------------------------
 for nodename in mpalyes01 mpalyes02 mpalyes03
 do
     echo "Filebeat BINARY SYNC es in $nodename -------------------------------------------------------------------------"
-    rsync -rv -e ssh --delete ~/filebeat/filebeat-$ES_VERSION-linux-x86_64-master root@$nodename:/home/root/filebeat
-    rsync -rv -e ssh --delete ~/filebeat/script root@$nodename:/home/root/filebeat
+    rsync -rv -e ssh --delete ~/filebeat/filebeat-$ES_VERSION-linux-x86_64-master root@$nodename:/root/filebeat
+    rsync -rv -e ssh --delete ~/filebeat/script root@$nodename:/root/filebeat
 done
 
 echo "-------------------------------------------------------------------end  `date +%Y/%m/%d-%H:%M`"
