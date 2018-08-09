2594797a8036711b6a48a42fb3a6ad664cd22fa4
Kwangsiklee <lks21c@gmail.com>
2018-08-09 17:54:15 +0900

    yes

--- filebeat-prd/symbolic_link.sh
+++ filebeat-prd/symbolic_link.sh
@@ -4,8 +4,10 @@ bin=`dirname "$this"`
 bin=`cd "$bin"; pwd`
 export SERVERLIST="${bin}/serverlist/serverlist_$1"
 
+. ../elasticsearch-prd/mpalyes/script/env.sh
+
 for server in `cat "$SERVERLIST"`; do
-  ssh root@$server "sh /root/filebeat/filebeat-6.3.2-linux-x86_64-$1 filebeat"
+  ssh root@$server "sh /root/filebeat/filebeat-$ES_VERSION-linux-x86_64-$1 filebeat"
 done
 
 wait
