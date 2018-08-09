8499add4da61e3859936252ac826caf855c96907
Kwangsiklee <lks21c@gmail.com>
2018-08-09 16:34:03 +0900

    yes

--- elasticsearch-prd/mpalyes/rsync_kibana.sh
+++ elasticsearch-prd/mpalyes/rsync_kibana.sh
@@ -4,7 +4,7 @@ echo ""
 echo ""
 echo "ES ALY BINARY SYNC START-------------------------------------------------------------------------"
 
-. ../elasticsearch-prd/mpalyes/script/env.sh
+. script/env.sh
 
 echo "ES ALY BINARY SYNC Kibana-------------------------------------------------------------------------"
 rsync -rv -e ssh --delete ~/elasticsearch/kibana-$KIBANA_VERSION-linux-x86_64 hadoop@mpalyes10:/home/hadoop/elasticsearch
