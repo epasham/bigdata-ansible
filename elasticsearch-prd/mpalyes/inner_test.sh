c0505afd96de672391ddd12eabe7af6d79fd95a2
Kwangsiklee <lks21c@gmail.com>
2018-08-10 09:48:45 +0900

    yes

--- elasticsearch-prd/mpalyes/inner_test.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ansible-playbook ansible/parent.yml -i ansible/local_inventory
--- elasticsearch-prd/mpalyes/local_rolling_restart.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ansible-playbook ansible/local_rolling_restart.yml -i ansible/local_inventory
