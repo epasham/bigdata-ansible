96cd4fc18ccc31d8c33c00046fd0d9e44c255d04
Kwangsiklee <lks21c@gmail.com>
2018-08-13 15:06:20 +0900

    add metricbeat

--- metricbeat-prd/rsync_metricbeat.sh
+++ metricbeat-prd/rsync_metricbeat.sh
@@ -22,6 +22,8 @@ export HOSTLIST="${bin}/serverlist/$role"
 for nodename in `cat "$HOSTLIST"`;
 do
     echo "Metricbeat BINARY SYNC in $nodename -------------------------------------------------------------------------"
+    ssh root@$nodename "rm -rf /root/metricbeat/metricbeat"
+    ssh root@$nodename "ln -s /root/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system-and-kafka /root/metricbeat/metricbeat"
     rsync -rv -e ssh --delete ~/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system-and-kafka root@$nodename:/root/metricbeat
     rsync -rv -e ssh --delete ~/metricbeat/script root@$nodename:/root/metricbeat
 done
