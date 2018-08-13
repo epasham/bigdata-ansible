54394921e31b048f81f289d1f26d474e32cef067
Kwangsiklee <lks21c@gmail.com>
2018-08-13 16:16:37 +0900

    add metricbeat

--- metricbeat-prd/rsync_metricbeat.sh
+++ metricbeat-prd/rsync_metricbeat.sh
@@ -38,11 +38,6 @@ export HOSTLIST="${bin}/serverlist/serverlist-system-and-zookeeper"
 for nodename in `cat "$HOSTLIST"`;
 do
     echo "Metricbeat BINARY SYNC in $nodename -------------------------------------------------------------------------"
-    # 임시 시작
-    ssh root@$nodename "rm -rf /root/metricbeat"
-    ssh root@$nodename "mkdir /root/metricbeat"
-    ssh root@$nodename "mv /root/metricbeat*_64 /root/metricbeat"
-    # 임시 끝
 
     ssh root@$nodename "rm -rf /root/metricbeat/metricbeat"
     ssh root@$nodename "ln -s /root/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system-and-zookeeper /root/metricbeat/metricbeat"
@@ -63,8 +58,8 @@ do
     # 임시 끝
 
     ssh root@$nodename "rm -rf /root/metricbeat/metricbeat"
-    ssh root@$nodename "ln -s /root/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-toros /root/metricbeat/metricbeat"
-    rsync -rv -e ssh --delete ~/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-toros root@$nodename:/root/metricbeat
+    ssh root@$nodename "ln -s /root/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system /root/metricbeat/metricbeat"
+    rsync -rv -e ssh --delete ~/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system root@$nodename:/root/metricbeat
     rsync -rv -e ssh --delete ~/metricbeat/script root@$nodename:/root/metricbeat
     ssh root@$nodename "chmod +x /root/metricbeat/script/*.sh"
 done
