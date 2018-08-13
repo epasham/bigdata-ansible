71980ee4501659bb099ffa239ff3061c6a249961
Kwangsiklee <lks21c@gmail.com>
2018-08-13 15:21:42 +0900

    add metricbeat

--- metricbeat-prd/rsync_metricbeat.sh
+++ metricbeat-prd/rsync_metricbeat.sh
@@ -9,13 +9,16 @@ echo "Metricbeat BINARY SYNC START----------------------------------------------
 
 . ../elasticsearch-prd/mpalyes/script/env.sh
 
-export HOSTLIST="${bin}/serverlist/serverlist-system"
-#for nodename in `cat "$HOSTLIST"`;
-#do
-#    echo "Metricbeat BINARY SYNC in $nodename -------------------------------------------------------------------------"
-#    rsync -rv -e ssh --delete ~/metricbeat/metricbeat-$metricbeat_VERSION-linux-x86_64-system root@$nodename:/root/metricbeat
-#    rsync -rv -e ssh --delete ~/metricbeat/script root@$nodename:/root/metricbeat
-#done
+export HOSTLIST="${bin}/serverlist/serverlist-mpalyes"
+for nodename in `cat "$HOSTLIST"`;
+do
+    echo "Metricbeat BINARY SYNC in $nodename -------------------------------------------------------------------------"
+    ssh root@$nodename "rm -rf /root/metricbeat/metricbeat"
+    ssh root@$nodename "ln -s /root/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system /root/metricbeat/metricbeat"
+    rsync -rv -e ssh --delete ~/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system root@$nodename:/root/metricbeat
+    rsync -rv -e ssh --delete ~/metricbeat/script root@$nodename:/root/metricbeat
+    ssh root@$nodename "chmod +x /root/metricbeat/script/*.sh"
+done
 
 export HOSTLIST="${bin}/serverlist/serverlist-system-and-kafka"
 
