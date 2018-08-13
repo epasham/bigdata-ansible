c03f292ba2aed68048c3b5356adb174f56d68860
Kwangsiklee <lks21c@gmail.com>
2018-08-13 15:34:27 +0900

    add metricbeat

--- metricbeat-prd/rsync_metricbeat.sh
+++ metricbeat-prd/rsync_metricbeat.sh
@@ -44,4 +44,22 @@ do
     ssh root@$nodename "chmod +x /root/metricbeat/script/*.sh"
 done
 
+export HOSTLIST="${bin}/serverlist/serverlist-system-and-zookeeper"
+
+for nodename in `cat "$HOSTLIST"`;
+do
+    echo "Metricbeat BINARY SYNC in $nodename -------------------------------------------------------------------------"
+    # 임시 시작
+    ssh root@$nodename "rm -rf /root/metricbeat"
+    ssh root@$nodename "mkdir /root/metricbeat"
+    ssh root@$nodename "mv /root/metricbeat*_64 /root/metricbeat"
+    # 임시 끝
+
+    ssh root@$nodename "rm -rf /root/metricbeat/metricbeat"
+    ssh root@$nodename "ln -s /root/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system-and-zookeeper /root/metricbeat/metricbeat"
+    rsync -rv -e ssh --delete ~/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system-and-zookeeper root@$nodename:/root/metricbeat
+    rsync -rv -e ssh --delete ~/metricbeat/script root@$nodename:/root/metricbeat
+    ssh root@$nodename "chmod +x /root/metricbeat/script/*.sh"
+done
+
 echo "-------------------------------------------------------------------end  `date +%Y/%m/%d-%H:%M`"
