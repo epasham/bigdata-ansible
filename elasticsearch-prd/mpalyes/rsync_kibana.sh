32fc69bb5a57c73f0249efbddc6d20bedba4d88e
Kwangsiklee <lks21c@gmail.com>
2018-08-14 14:31:48 +0900

    refactor kibana

--- elasticsearch-prd/mpalyes/rsync_kibana.sh
+++ elasticsearch-prd/mpalyes/rsync_kibana.sh
@@ -7,14 +7,16 @@ echo "ES ALY BINARY SYNC START--------------------------------------------------
 . script/env.sh
 
 export ROLE=plain
+export NODE=mpalyes10
 echo "ES ALY BINARY SYNC Kibana-------------------------------------------------------------------------"
-ssh hadoop@mpalyes10 "rm -rf /home/hadoop/elasticsearch/kibana"
-ssh hadoop@mpalyes10 "ln -s /home/hadoop/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 /home/hadoop/elasticsearch/kibana"
-rsync -rv -e ssh --delete ~/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 hadoop@mpalyes10:/home/hadoop/elasticsearch
+ssh hadoop@$NODE "rm -rf /home/hadoop/elasticsearch/kibana"
+ssh hadoop@$NODE "ln -s /home/hadoop/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 /home/hadoop/elasticsearch/kibana"
+rsync -rv -e ssh --delete ~/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 hadoop@$NODE:/home/hadoop/elasticsearch
 
 export ROLE=esc
-ssh hadoop@mpalyes11 "rm -rf /home/hadoop/elasticsearch/kibana"
-ssh hadoop@mpalyes11 "ln -s /home/hadoop/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 /home/hadoop/elasticsearch/kibana"
-rsync -rv -e ssh --delete ~/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 hadoop@mpalyes11:/home/hadoop/elasticsearch
+export NODE=mpalyes11
+ssh hadoop@$NODE "rm -rf /home/hadoop/elasticsearch/kibana"
+ssh hadoop@$NODE "ln -s /home/hadoop/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 /home/hadoop/elasticsearch/kibana"
+rsync -rv -e ssh --delete ~/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 hadoop@$NODE:/home/hadoop/elasticsearch
 
 echo "-------------------------------------------------------------------end  `date +%Y/%m/%d-%H:%M`"
