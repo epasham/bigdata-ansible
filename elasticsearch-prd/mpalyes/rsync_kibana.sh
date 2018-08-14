9f07e1a50e4058cdcc0d2ca3b4ed2105d67d312d
Kwangsiklee <lks21c@gmail.com>
2018-08-14 16:38:31 +0900

    test

--- elasticsearch-prd/mpalyes/rsync_kibana.sh
+++ elasticsearch-prd/mpalyes/rsync_kibana.sh
@@ -13,7 +13,7 @@ ssh hadoop@$NODE "rm -rf /home/hadoop/elasticsearch/kibana"
 ssh hadoop@$NODE "ln -s /home/hadoop/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 /home/hadoop/elasticsearch/kibana"
 rsync -rv -e ssh --delete ~/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 hadoop@$NODE:/home/hadoop/elasticsearch
 
-export ROLE=esc
+export ROLE=plain
 export NODE=mpalyes11
 ssh hadoop@$NODE "rm -rf /home/hadoop/elasticsearch/kibana"
 ssh hadoop@$NODE "ln -s /home/hadoop/elasticsearch/kibana-$KIBANA_VERSION-$ROLE-linux-x86_64 /home/hadoop/elasticsearch/kibana"
