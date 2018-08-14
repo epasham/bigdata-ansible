947276ad3634864a092ed6dc734cd43ca4303499
Kwangsiklee <lks21c@gmail.com>
2018-08-14 13:43:07 +0900

    refactor kibana

--- elasticsearch-prd/mpalyes/rsync_kibana.sh
+++ elasticsearch-prd/mpalyes/rsync_kibana.sh
@@ -7,7 +7,12 @@ echo "ES ALY BINARY SYNC START--------------------------------------------------
 . script/env.sh
 
 echo "ES ALY BINARY SYNC Kibana-------------------------------------------------------------------------"
-rsync -rv -e ssh --delete ~/elasticsearch/kibana-$KIBANA_VERSION-linux-x86_64 hadoop@mpalyes10:/home/hadoop/elasticsearch
-rsync -rv -e ssh --delete ~/elasticsearch/kibana-$KIBANA_VERSION-linux-x86_64 hadoop@mpalyes11:/home/hadoop/elasticsearch
+ssh hadoop@mpalyes10 "rm -rf /home/hadoop/elasticsearch/kibana"
+ssh hadoop@mpalyes10 "ln -s /home/hadoop/elasticsearch/kibana-$KIBANA_VERSION-esc-linux-x86_64 /home/hadoop/elasticsearch/kibana"
+rsync -rv -e ssh --delete ~/elasticsearch/kibana-$KIBANA_VERSION-esc-linux-x86_64 hadoop@mpalyes10:/home/hadoop/elasticsearch
+
+ssh hadoop@mpalyes11 "rm -rf /home/hadoop/elasticsearch/kibana"
+ssh hadoop@mpalyes11 "ln -s /home/hadoop/elasticsearch/kibana-$KIBANA_VERSION-esc-linux-x86_64 /home/hadoop/elasticsearch/kibana"
+rsync -rv -e ssh --delete ~/elasticsearch/kibana-$KIBANA_VERSION-plain-linux-x86_64 hadoop@mpalyes11:/home/hadoop/elasticsearch
 
 echo "-------------------------------------------------------------------end  `date +%Y/%m/%d-%H:%M`"
