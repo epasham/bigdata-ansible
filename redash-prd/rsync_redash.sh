b3f53a29f6a73e2e12c3f57a25ca38d4d5b86482
Kwangsiklee <lks21c@gmail.com>
2018-08-16 11:34:58 +0900

    change to 4.0.1

--- redash-prd/rsync_redash.sh
+++ redash-prd/rsync_redash.sh
@@ -4,11 +4,14 @@ echo ""
 echo ""
 echo "Redash BINARY SYNC START-------------------------------------------------------------------------"
 
+export REDASH_VERSION=4.0.1
+
 for nodename in mpredash mpredash02
 do
     echo "Redash SYNC in $nodename -------------------------------------------------------------------------"
     rsync -rv -e ssh --delete script/*.sh suser@$nodename:/home/suser/redash
     rsync -rv -e ssh --delete script/Dockerfile suser@$nodename:/home/suser/redash
+    ssh suser@nodename "docker pull lks21c/redash:$REDASH_VERSION"
     ssh suser@nodename "chmod +x /home/suser/redash/*.sh"
 done
 
