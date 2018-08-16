c025101f662b9876272741d1a00f79de6a3f7f94
Kwangsiklee <lks21c@gmail.com>
2018-08-16 11:39:02 +0900

    change to 4.0.1

--- redash-prd/rsync_redash.sh
+++ redash-prd/rsync_redash.sh
@@ -11,8 +11,8 @@ do
     echo "Redash SYNC in $nodename -------------------------------------------------------------------------"
     rsync -rv -e ssh --delete script/*.sh suser@$nodename:/home/suser/redash
     rsync -rv -e ssh --delete script/Dockerfile suser@$nodename:/home/suser/redash
-    ssh suser@nodename "docker pull lks21c/redash:$REDASH_VERSION"
-    ssh suser@nodename "chmod +x /home/suser/redash/*.sh"
+    ssh suser@$nodename "docker pull lks21c/redash:$REDASH_VERSION"
+    ssh suser@$nodename "chmod +x /home/suser/redash/*.sh"
 done
 
 echo "-------------------------------------------------------------------end  `date +%Y/%m/%d-%H:%M`"
