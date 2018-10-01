2de97387b510d7c9318f4bd72037fd42a111a442
Kwangsik Lee <lks21c@gmail.com>
2018-10-01 09:02:52 +0900

    yes

--- jdk/install_jdk_on_centos.sh
+++ jdk/install_jdk_on_centos.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd /tmp && wget http://www.kwangsiklee.com/wp-content/uploads/direct/jdk-8u181-linux-x64.tar.gz
-cd ~/repo/bigdata-ansible/jdk && ansible-playbook ansible/jdk.yml --become --ask-sudo-pass
+cd /tmp && rm -rf jdk-8u181-linux-x64.tar.gz && wget http://www.kwangsiklee.com/wp-content/uploads/direct/jdk-8u181-linux-x64.tar.gz
+cd ~/repo/bigdata-ansible/jdk && ansible-playbook ansible/jdk.yml --become
