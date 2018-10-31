53a8415b964df036e02a92beed4a136b859018d9
Kwangsik Lee <lks21c@gmail.com>
2018-10-31 11:36:12 +0900

    add mpdstg

--- metricbeat-prd/rsync_metricbeat.sh
+++ metricbeat-prd/rsync_metricbeat.sh
@@ -100,4 +100,22 @@ do
     ssh root@$nodename "chmod +x /root/metricbeat/script/*.sh"
 done
 
+export HOSTLIST="${bin}/serverlist/serverlist-mpdstg"
+
+for nodename in `cat "$HOSTLIST"`;
+do
+    echo "Metricbeat BINARY SYNC in $nodename -------------------------------------------------------------------------"
+    # temp start
+#    ssh root@$nodename "rm -rf /root/metricbeat"
+#    ssh root@$nodename "mkdir /root/metricbeat"
+#    ssh root@$nodename "mv /root/metricbeat*_64 /root/metricbeat"
+    # temp end
+
+    ssh root@$nodename "rm -rf /root/metricbeat/metricbeat"
+    ssh root@$nodename "ln -s /root/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system /root/metricbeat/metricbeat"
+    rsync -rv -e ssh --delete ~/metricbeat/metricbeat-$METRICBEAT_VERSION-linux-x86_64-system root@$nodename:/root/metricbeat
+    rsync -rv -e ssh --delete ~/metricbeat/script root@$nodename:/root/metricbeat
+    ssh root@$nodename "chmod +x /root/metricbeat/script/*.sh"
+done
+
 echo "-------------------------------------------------------------------end  `date +%Y/%m/%d-%H:%M`"
