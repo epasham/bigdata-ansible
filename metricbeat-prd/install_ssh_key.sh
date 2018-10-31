aeafdb622984c3dd056fbb76ece62f373bb994c2
Kwangsik Lee <lks21c@gmail.com>
2018-10-31 10:45:10 +0900

    install_ssh_key 추가

--- metricbeat-prd/install_ssh_key.sh
+++ metricbeat-prd/install_ssh_key.sh
@@ -6,7 +6,7 @@ bin=`cd "$bin"; pwd`
 export HOSTLIST="${bin}/serverlist/serverlist-$1"
 for nodename in `cat "$HOSTLIST"`;
 do
-  echo "nodename installed"
+  echo "Installed $nodename -------------------------------------------------------------------------"
   ssh root@nodename "echo 'ssh_key' >> ~/.ssh/authorized_keys"
 done
 
